e0c0a005afc982dbaeb083dd8247729b6628922b
sunantha2727 <tha-fay@hotmail.co.th>
2017-12-13 11:09:01 +0700

    TestArrayList

---
+++ src/ooplab5/TestArrayList.java
@@ -0,0 +1,27 @@
+package ooplab5;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class TestArrayList {
+    public static void main(String[] args) {
+    //ArrayList
+        ArrayList list = new ArrayList();
+        list.add("Fai");
+        list.add("Bow");
+        list.add("Dam");
+        System.out.println(list);
+        System.out.println(list.size());
+        list.remove(1);
+        System.out.println(list);
+        list.add(1,"YAYA");
+        System.out.println(list);
+        //print with for loop
+        for (int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }//for i
+        System.out.println(list.indexOf("YAYA"));
+        list.set(1,"BABY");
+        System.out.println(list);
+    }//main
+}//class
