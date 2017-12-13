72d11ff253e4344f354ad209aaab4406044195b1
sunantha2727 <tha-fay@hotmail.co.th>
2017-12-13 09:51:34 +0700

    TestWhileLoop

---
+++ src/ooplab4/TestWhileLoop.java
@@ -0,0 +1,21 @@
+package ooplab4;
+
+public class TestWhileLoop {
+    public static void main(String[] args) {
+        //Whle
+        int n = 1;
+        while (n<=10){
+            System.out.println(n+" ");
+            n++;
+
+        }//While
+
+
+
+
+    }//main
+
+
+
+
+}//class
