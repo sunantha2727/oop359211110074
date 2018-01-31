58779f2877132b047bd5bacf31911756a3124e7c
tha-fay@hotmail.co.th <tha-fay@hotmail.co.th>
2018-01-31 09:13:40 +0700

    Inherritance person

---
+++ src/oopleb8/Person.java
@@ -0,0 +1,63 @@
+package oopleb8;
+
+import java.util.Date;
+
+public class Person {
+    private String name;
+    private String gander;
+    private Date dateOFBirth;
+    private int age;
+
+    public Person(String name, String gander, Date dateOFBirth, int age) {
+        this.name = name;
+        this.gander = gander;
+        this.dateOFBirth = dateOFBirth;
+        this.age = age;
+    }
+    //toString()
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", gander='" + gander + '\'' +
+                ", dateOFBirth=" + dateOFBirth +
+                ", age=" + age +
+                '}';
+    }
+
+    //getter and setter methods
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGander() {
+        return gander;
+    }
+
+    public void setGander(String gander) {
+        this.gander = gander;
+    }
+
+    public Date getDateOFBirth() {
+        return dateOFBirth;
+    }
+
+    public void setDateOFBirth(Date dateOFBirth) {
+        this.dateOFBirth = dateOFBirth;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
