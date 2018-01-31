356a4b6b2aa7b60ee9f33297117aff396015047e
tha-fay@hotmail.co.th <tha-fay@hotmail.co.th>
2018-01-31 10:50:42 +0700

    PetApp

--- src/oopleb8/PetApp.java
+++ src/oopleb8/PetApp.java
@@ -1,6 +1,6 @@
 package oopleb8;
 
-public class PetApp {
+public class  PetApp {
     public static void main(String[] args) {
         Dog dog = new Dog("Chayen", 2);
         System.out.println(dog.toString());
