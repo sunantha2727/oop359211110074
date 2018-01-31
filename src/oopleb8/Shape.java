a225bb64c6af768cc571dcefeb9a66cf5c6a4419
tha-fay@hotmail.co.th <tha-fay@hotmail.co.th>
2018-01-31 11:11:52 +0700

    Shape

---
+++ src/oopleb8/Shape.java
@@ -0,0 +1,6 @@
+package oopleb8;
+
+public interface Shape {
+    double getArea();
+
+}
---
+++ src/oopleb8/ShapeApp.java
@@ -0,0 +1,9 @@
+package oopleb8;
+
+public class ShapeApp {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(30, 30 );
+        System.out.println(rectangle.toString());
+        System.out.println("Oject area : "+rectangle.getArea());
+    }
+}
