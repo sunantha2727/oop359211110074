146c6560106c9d18699daafbfcbab388b915ae1a
tha-fay@hotmail.co.th <tha-fay@hotmail.co.th>
2018-01-31 09:33:08 +0700

    studen

---
+++ src/oopleb8/Student.java
@@ -0,0 +1,44 @@
+package oopleb8;
+
+import java.util.Date;
+//student is sub-class
+//Person is super-class
+//Pweson is super-class
+
+public class Student extends Person {
+private String id;
+private String program;
+    public Student(String name, String gander, Date dateOFBirth, int age,String id, String program) {
+        super(name, gander, dateOFBirth, age);
+        this.id = id;
+        this.program = program;
+    }
+    //toSting ()
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", program='" + program + '\'' +
+                "} " + super.toString();
+    }
+
+    //getter and setter method
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+}//calss
